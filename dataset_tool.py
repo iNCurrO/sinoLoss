272877dbb57b0c407114c36593cab54be7b153a8
Gihun Kim <rlarlgns0514@naver.com>
2023-03-15 13:42:51 +0900

    Dog Honey

--- dataset_tool.py
+++ dataset_tool.py
@@ -5,10 +5,12 @@ import zipfile
 
 
 def main():
-    url = 'https://drive.google.com/uc?id=1lb3TOWIOcJy1g5OXqfw_CIic43Jf2OJC'
+    url = 'https://drive.google.com/uc?id=19lGLrBsIZfUfhOaGTv29cbWRGTiX50_z'
     url_observerloss = 'https://drive.google.com/uc?id=1EOJMX4g1E0m3CLbf4BiPUh_RUm2N_FTK'
     print("Parse configurations")
     config = get_config()
+    if not os.path.exists(os.path.join(config.datadir)):
+        os.mkdir(os.path.join(config.datadir))
     gdown.download(url, os.path.join(config.datadir, "downloadedfromGD"), fuzzy=True)
     gdown.download(url_observerloss, os.path.join(config.datadir, "observerloss.h5"), fuzzy=True)
     with zipfile.ZipFile(os.path.join(config.datadir, "downloadedfromGD"), 'r') as zip_ref:
